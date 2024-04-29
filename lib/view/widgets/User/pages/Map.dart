0f8810cf4076de0933c48a6d2dd094b6224428c9
Hassan <hassandurra81@gmail.com>
2024-04-30 03:03:35 +0500

    fixed some issues in map

--- lib/view/widgets/User/pages/Map.dart
+++ lib/view/widgets/User/pages/Map.dart
@@ -1,11 +1,10 @@
-import 'package:cityquest/assets/colors.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:cityquest/view/widgets/User/pages/map_inputs/map_api_credientals.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
@@ -15,13 +14,13 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  List<LatLng> Points = [];
+  List<LatLng> points = [];
   late LatLng currentLocation = LatLng(0.0, 0.0);
   late String? currentLongitudeRoute;
   late String? currentlatitudeRoute;
-  late LatLng destinationLocation = LatLng(24.8755064,67.0410023);
+  late LatLng destinationLocation = LatLng(24.8755064, 67.0410023);
   bool showCurrentLocation = false;
-  late double zoomLevel = 15.0;
+  double zoomLevel = 15.0;
   final TextEditingController destinationController = TextEditingController();
 
   @override
@@ -31,7 +30,7 @@ class _MapViewState extends State<MapView> {
     getCordinates();
   }
 
-  void getCurrentLocation() async {
+  getCurrentLocation() async {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied ||
         permission == LocationPermission.deniedForever) {
@@ -52,6 +51,7 @@ class _MapViewState extends State<MapView> {
   }
 
   getCordinates() async {
+    points.clear();
     var response = await http.get(getRouteUrl(
         "${currentlatitudeRoute},${currentLongitudeRoute}",
         "24.8755064,67.0410023"));
@@ -62,7 +62,7 @@ class _MapViewState extends State<MapView> {
         for (int i = 0; i < data[0].length; i++) {
           double long = data[0][i][1];
           double lat = data[0][i][0];
-          Points.add(LatLng(long, lat));
+          points.add(LatLng(long, lat));
         }
       }
     });
@@ -74,131 +74,108 @@ class _MapViewState extends State<MapView> {
       body: Stack(
         children: [
           if (showCurrentLocation)
-            FlutterMap(
-              options: MapOptions(
-                minZoom: zoomLevel,
-                maxZoom: zoomLevel,
-                initialZoom: zoomLevel,
-                initialCenter: currentLocation,
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate:
-                      'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=914dea0a4e48423e8af1c0ac35cac48a',
-                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            GestureDetector(
+              onScaleUpdate: (details) {
+                setState(() {
+                  zoomLevel -= details.scale / 10;
+                });
+              },
+              child: FlutterMap(
+                key: ValueKey(currentLocation),
+                options: MapOptions(
+                  minZoom: zoomLevel,
+                  maxZoom: zoomLevel,
+                  initialZoom: zoomLevel,
+                  initialCenter: currentLocation,
                 ),
-                MarkerLayer(
-                  markers: [
-                    Marker(
-                      point: currentLocation,
-                      width: 80,
-                      height: 80,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.green,
-                          size: 40,
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=914dea0a4e48423e8af1c0ac35cac48a',
+                    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                  ),
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: currentLocation,
+                        width: 80,
+                        height: 80,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.location_on,
+                            color: Colors.green,
+                            size: 40,
+                          ),
                         ),
                       ),
-                    ),
-                    Marker(
-                      point: destinationLocation,
-                      width: 80,
-                      height: 80,
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.red,
-                          size: 40,
+                      Marker(
+                        point: destinationLocation,
+                        width: 80,
+                        height: 80,
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                            size: 40,
+                          ),
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                if (Points.isNotEmpty)
-                  PolylineLayer(
-                    polylines: [
-                      Polyline(
-                        points: Points,
-                        color: Colors.blue,
-                        strokeWidth: 5,
                       ),
                     ],
                   ),
-              ],
+                  if (points.isNotEmpty)
+                    PolylineLayer(
+                      polylines: [
+                        Polyline(
+                          points: points,
+                          color: Colors.blue,
+                          strokeWidth: 5,
+                        ),
+                      ],
+                    ),
+                ],
+              ),
             ),
-
-            Positioned(
-  top: 16,
-  left: 16,
-  right: 16,
-  child: Container(
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 5,
-          blurRadius: 7,
-          offset: Offset(0, 3),
-        ),
-      ],
-    ),
-    child: Row(
-      children: [
-        Expanded(
-          child: TextField(
-            controller: destinationController,
-            decoration: InputDecoration(
-              hintText: 'Enter Destination',
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(horizontal: 16),
+          Positioned(
+            top: 16,
+            left: 16,
+            right: 16,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: destinationController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Destination',
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Perform search
+                    },
+                    icon: Icon(Icons.search),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        IconButton(
-          onPressed: () {
-            // Perform search
-          },
-          icon: Icon(Icons.search),
-        ),
-      ],
-    ),
-  ),
-),
-
-          // Positioned(
-          //   top: 16,
-          //   left: 16,
-          //   right: 16,
-          //   child: Container(
-          //     child: Row(
-          //       children: [
-          //         Expanded(
-          //           child: TextField(
-          //             controller: destinationController,
-          //             decoration: InputDecoration(
-          //               hintText: 'Enter destination',
-          //               border: OutlineInputBorder(),
-          //             ),
-          //           ),
-          //         ),
-          //         SizedBox(
-          //           width: 20,
-          //         ),
-          //         IconButton(
-          //           onPressed: () {
-          //             // Perform search
-          //           },
-          //           icon: Icon(Icons.search),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // ),
           Positioned(
             right: 16,
             bottom: 16,
@@ -209,32 +186,28 @@ class _MapViewState extends State<MapView> {
                   onPressed: () {
                     getCurrentLocation();
                   },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    elevation: MaterialStateProperty.all<double>(8.0),
+                    backgroundColor: Colors.white,
+                    elevation: 8.0,
                   ),
-                  child: Icon(Icons.location_on, color: GlobalColors.mainColor),
+                  child: Icon(Icons.location_on, color: Colors.green),
                 ),
                 SizedBox(height: 10),
                 ElevatedButton(
                   onPressed: () {
                     getCordinates();
                   },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    elevation: MaterialStateProperty.all<double>(8.0),
+                    backgroundColor: Colors.white,
+                    elevation: 8.0,
                   ),
-                  child: Icon(Icons.directions, color: GlobalColors.mainColor),
+                  child: Icon(Icons.directions, color: Colors.red),
                 ),
                 SizedBox(height: 10),
                 ElevatedButton(
@@ -243,16 +216,14 @@ class _MapViewState extends State<MapView> {
                       zoomLevel += 1.0;
                     });
                   },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    elevation: MaterialStateProperty.all<double>(8.0),
+                    backgroundColor: Colors.white,
+                    elevation: 8.0,
                   ),
-                  child: Icon(Icons.add, color: GlobalColors.mainColor),
+                  child: Icon(Icons.add, color: Colors.blue),
                 ),
                 SizedBox(height: 10),
                 ElevatedButton(
@@ -261,16 +232,14 @@ class _MapViewState extends State<MapView> {
                       zoomLevel -= 1.0;
                     });
                   },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    elevation: MaterialStateProperty.all<double>(8.0),
+                    backgroundColor: Colors.white,
+                    elevation: 8.0,
                   ),
-                  child: Icon(Icons.remove, color: GlobalColors.mainColor),
+                  child: Icon(Icons.remove, color: Colors.blue),
                 ),
               ],
             ),
